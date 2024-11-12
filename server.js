a6eea88d9535a68ad02b0b7267b07ec23e1a76aa
xoten herself <ana.schotten@estudante.ifms.edu.br>
2024-11-12 00:16:07 -0300

    .

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-var userRoute = require('./userRoute.js');
 
-app.use('/', userRoute)
+const requestTime = function (req, res, next) {
+    req.requestTime = Date.now()
+    next()
+}
+
+app.use(requestTime)
+
+app.get('/', (req, res) => {
+    let responseText = `'Hello World! Requested at: '${req.requestTime}`
+    res.send(responseText)
+})
 
 app.listen(port, () => {
     console.log('Servidor Rodando...');
